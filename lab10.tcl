4a277f91587131e360d6a207eea59a6f5aa860f8
Suhani S K <156022230+SuhaniSK@users.noreply.github.com>
2025-03-24 09:08:03 +0530

    Update lab10.tcl

--- lab10.tcl
+++ lab10.tcl
@@ -1,4 +1,4 @@
-# 4. Implement simple ESS and with transmitting nodes in wire-less LAN by simulation
+# 10. Implement simple ESS and with transmitting nodes in wire-less LAN by simulation
 # and determine the performance with respect to transmission of packets.
 set ns [new Simulator]
 set na [open Lab4.nam w]
