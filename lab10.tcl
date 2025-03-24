36412926e61406a1185cf27abd2bdcfbf6ec610f
Suhani S K <156022230+SuhaniSK@users.noreply.github.com>
2025-03-24 11:17:30 +0530

    Update lab10.tcl

--- lab10.tcl
+++ lab10.tcl
@@ -67,16 +67,4 @@ $ns at 10.0 "End"
 $ns run
  
  
-AWK Code
-|
-BEGIN{Num_of_pkts=0;}
-{
-if ($1 == "r" && $3 == "_1_" && $4 == "AGT" && $7 == "tcp")
-{
-Num_of_pkts = Num_of_pkts + $8;
-}
-}
-END{
-Throughput = Num_of_pkts * 8 / $2 /1000000;
-printf("\n\n\tThroughput = %fbpms\n\n\n",Throughput);
-}
+# Awk Code is in lab10b.awk file
